ff7fe64a042ce1c66bd165af079378dd2250fb99
myinfo0196 <130123610+myinfo0196@users.noreply.github.com>
2024-11-19 18:01:40 +0900

    three commit

--- components/w_ac01040_01.js
+++ components/w_ac01040_01.js
@@ -154,7 +154,7 @@ const w_ac01040_01 = ({ item = {}, isOpen, onClose, onSave, mode, title }) => {
 
   return (
     <ModalBackground>
-      <Draggable handle=".modal-header" cancel="button"> {/* 버튼 클릭 시 드래그 방지 */}
+      <Draggable handle=".modal-header" cancel="button">
         <ModalContent>
           <ModalHeader className="modal-header" onMouseDown={e => e.stopPropagation()}>
             <h2>{title}</h2>
